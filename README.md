ac2d6ce935454fa4ff5221513a1185e165513662
myeongin <mungin0926@naver.com>
2023-03-01 21:42:39 +0900

    finish

---
+++ README.md
@@ -0,0 +1,18 @@
+## Movie-Search
+
+HTML,CSS,JavaScript,Node.js로 제작되었습니다.  
+반응형 웹 페이지로 pc,태블릿,모바일에서도 안정적인 화면을 제공합니다.
+
+[Demo](https://movie-search-gsjv.vercel.app/#/) - 바로가기
+<img src="./main-image.PNG" alt="main" />
+<img src="./movie-image.PNG" alt="main" />
+
+## API
+
+OMDb API를 사용해서 제작하였습니다.
+
+## 기능
+
+- 찾고자 하는 영화의 제목을 영문으로 작성하여 검색할 수 있습니다.
+
+- 포스터를 클릭시 영화의 상세 정보를 볼 수 있습니다
--- src/store/movie.js
+++ src/store/movie.js
@@ -20,9 +20,12 @@ export const searchMovies = async (page) => {
     store.state.message = "";
   }
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=9fa836f9&s=${store.state.searchText}&page=${page}`
-    );
+    const res = await fetch("./api/movie", {
+      method: "POST",
+      body: JSON.stringify({ 
+        title: store.state.searchText, 
+        page }),
+    });
     const { Search, totalResults, Response, Error } = await res.json();
 
     if (Response === "True") {
@@ -40,9 +43,10 @@ export const searchMovies = async (page) => {
 
 export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(
-      `https://omdbapi.com?apikey=9fa836f9&i=${id}&plot=full`
-    );
+    const res = await fetch("/api/movie", {
+      method: "POST",
+      body: JSON.stringify({ id }),
+    });
     store.state.movie = await res.json();
   } catch (error) {
     console.log("getMovieDetails error:", error);
